527661b0691614dc546c67dbfaa36d743ab8aac9
Rithy <RithyBHuot@gmail.com>
2018-06-19 19:45:33 -0400

    more setup

---
+++ src/components/shared/modalStyle.js
@@ -0,0 +1,32 @@
+const style = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.54)',
+    zIndex: 10
+  },
+  content: {
+    border: '0',
+    borderRadius: '1px',
+    bottom: 'auto',
+    minHeight: '25rem',
+    maxHeight: '35rem',
+    height: '100%',
+    left: '50%',
+    padding: '0.75rem',
+    position: 'fixed',
+    right: 'auto',
+    top: '45%',
+    transform: 'translate(-50%,-50%)',
+    minWidth: '10rem',
+    width: '80%',
+    maxWidth: '35rem',
+    backgroundColor: 'white',
+    boxShadow: '3px 3px 3px rgba(77, 73, 74, 0.52)'
+  }
+};
+
+export default style;
