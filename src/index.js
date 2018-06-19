0ffb4d622266dc38bccaa99e33885646584c57ca
Rithy <RithyBHuot@gmail.com>
2018-06-19 19:52:14 -0400

    setup app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ReactModal from './react-component';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import {  configureStore, history } from './store/store';
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactModal.setAppElement(document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./components/app', () => {
+    // const NextApp = require('./components/app').default;
+    ReactDOM.render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>,
+      document.getElementById('root')
+    );
+  });
+  window.store = store;
+}
+
 registerServiceWorker();
