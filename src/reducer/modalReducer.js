2aa641c800286bd87b690381fc64e0a60c49c4e6
Rithy <RithyBHuot@gmail.com>
2018-06-20 10:40:29 -0400

    clean up

--- src/reducer/modalReducer.js
+++ src/reducer/modalReducer.js
@@ -1,10 +1,9 @@
-import { RECEIVE_MAP_STATE, RECEIVE_THREAD_STATE } from '../actions/modalActions';
+import { RECEIVE_MAP_STATE } from '../actions/modalActions';
 import merge from 'lodash/merge';
 
 const defaultState = {
   modalState: {
-    location: nul,
-    thread: null
+    location: null
   }
 }
 
@@ -13,9 +12,6 @@ export const modalReducer = (state = defaultState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_MAP_STATE:
-      newState = merge({}, state, { modalState: action.modalState });
-      return newState;
-    case RECEIVE_THREAD_STATE:
       newState = merge({}, state, { modalState: action.modalState });
       return newState;
     default:
