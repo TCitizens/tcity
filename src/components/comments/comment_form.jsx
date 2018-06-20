db4ba4cb476fd18306b9947a2fc87459c2c729ae
Rhossan <rhossan93@gmail.com>
2018-06-20 11:14:16 -0400

    comment form Component created

---
+++ src/components/comments/comment_form.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class CommentForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const comment = Object.assign({}, this.state, { thread_id: this.props.threadId, username: this.props.currentUser.username });
+    this.props.makeComment(comment);
+    this.setState({ body:"" });
+  }
+
+  update(field) {
+    return e => this.setState({[field]: e.currentTarget.value});
+  }
+
+
+  render(){
+    return (
+      <div className='footer'>
+        <div className='footer_message'>
+          <form onSubmit={this.handleSubmit}>
+            <label className="label-message">
+              <input type="text"
+                placeholder={"Comment"}
+                value={this.state.body}
+                className="message-input"
+                onChange={this.update('body')}
+              />
+            </label>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default (CommentForm);
